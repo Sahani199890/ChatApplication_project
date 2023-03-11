f38cd5ab045d11bced5f27519374292a2c21cade
Sahani199890 <110148973+Sahani199890@users.noreply.github.com>
2023-03-11 21:08:39 +0530

    Delete demo/demo directory

--- demo/demo/src/main/java/com/example/demo/dao/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product,Integer> {
-}
